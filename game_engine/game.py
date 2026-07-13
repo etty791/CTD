defbc2e0cb2d300088589ccc55b613da824e3cc3
Etty <etty70910@gmail.com>
2026-07-13 09:52:02 +0300

    moving game to folder and taking out non-related functions

---
+++ game_engine/game.py
@@ -0,0 +1,30 @@
+from model.board import Board
+from rules.rules_engine import MoveValidation, validate_move
+from real_time.real_time_arbiter import RealTimeArbiter
+
+class KungFuChessGame:
+    def __init__(self, initial_board):
+        self.board = Board(initial_board)
+        self.rta=RealTimeArbiter(self.board)
+        self.game_active = True   
+
+    def move_request(self, origin, target):
+        if origin == target:
+            return False
+        if not self.game_active:
+            return MoveValidation(False, reason="game_over")
+        is_valid_move = validate_move(self.board, origin, target) 
+        if is_valid_move.is_valid:
+            if not self.rta.add_move(self.board.get_piece_at(origin), origin, target):
+                return MoveValidation(False, reason="invalid_move")
+            return MoveValidation(True, "ok")
+        return MoveValidation(False, is_valid_move.reason)
+    
+
+    def finish_game(self):
+        self.game_active = False
+
+    def wait(self, ms):
+        king_captured = self.rta.advance_time(ms)
+        if king_captured:
+            self.finish_game()
