0e5cea430a2b45bfb58d002daab4a68b3b81f9dd
Etty <etty70910@gmail.com>
2026-07-13 09:52:23 +0300

    cleaning main

--- main.py
+++ main.py
@@ -1,32 +1,8 @@
-VALID_TOKENS = {
-    '.',
-    'wK', 'wQ', 'wR', 'wB', 'wN', 'wP',
-    'bK', 'bQ', 'bR', 'bB', 'bN', 'bP'
-}
-from parse_clean import parse_input 
-from game import KungFuChessGame 
-from board import Board
+from textTester.script_runner import run_script
 
 def main():
-    board, commands = parse_input(VALID_TOKENS)
-    game = KungFuChessGame(Board(board))
-    
-    for cmd in commands:
-        parts = cmd.split()
-        action = parts[0]
-        
-        if action == "click":
-            x = int(parts[1])
-            y = int(parts[2])
-            game.handle_click(x, y)
-        elif action == "wait":
-            ms = int(parts[1])
-            game.handle_wait(ms)
-        elif action == "print":
-            if parts[1] == "board":
-                game.board.print_board()
-        else:
-            print("ERROR UNKNOWN_TOKEN")
+    run_script()
 
 if __name__ == "__main__":
     main()
+
