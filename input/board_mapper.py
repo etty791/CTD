bd80479039f26a749fac14dd395cc46adf744f87
Etty <etty70910@gmail.com>
2026-07-16 13:09:30 +0300

    board mapper from pixels to logical cells and vice versa

--- input/board_mapper.py
+++ input/board_mapper.py
@@ -1,3 +1,22 @@
-PIXELS_PER_CELL = 100
-def transfer_pixels_to_data(pixels):
-    return pixels//PIXELS_PER_CELL
+from view.view_config import *  
+class BoardMapper:
+    def __init__(self,board_width, board_height):
+        self.board_width = board_width
+        self.board_height=board_height
+    def pixels_to_logic(self,x, y):
+        """מתרגם קואורדינטות של פיקסלים לאינדקסים של מטריצת הלוח"""
+        col = (x * DEFAULT_BOARD_SIZE) // self.board_width
+        row = (y * DEFAULT_BOARD_SIZE) // self.board_height
+        return row, col
+        
+    
+    def logic_to_pixels(self,row, col):
+        """(אופציונלי) מתרגם אינדקס של מטריצה לנקודת ההתחלה של הפיקסלים - שימושי לציור"""
+        x = (col * self.board_width) //DEFAULT_BOARD_SIZE
+        y = (row * self.board_height) //DEFAULT_BOARD_SIZE
+        return x, y
+    
+
+
+
+
