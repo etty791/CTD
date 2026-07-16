a545dc2eb2479c315b7caa4a1f9c02990a9e2ac4
Etty <etty70910@gmail.com>
2026-07-16 13:08:29 +0300

    add the game loop

---
+++ view/game_loop.py
@@ -0,0 +1,52 @@
+import cv2
+import time
+
+MS_PER_SECOND = 1000
+WAIT_TICK_INTERVAL_SECONDS = 1.0
+FRAME_DELAY_MS = 33
+
+class GameLoop:
+    def __init__(self, game, asset_manager, renderer, controller):
+        self.game = game
+        self.asset_manager = asset_manager
+        self.renderer = renderer
+        self.controller = controller
+
+        self.window_name = "Game Window"
+        self.running = True
+
+        cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
+
+        cv2.setMouseCallback(self.window_name, self._mouse_callback)
+
+        self.last_wait_time = time.time()
+
+    def _mouse_callback(self, event, x, y, flags, param):
+       
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.controller.handle_click(x, y)
+            
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            self.controller.handle_jump(x, y)
+
+    def run(self):
+        while self.running:
+            current_time = time.time()
+
+            if current_time - self.last_wait_time >= WAIT_TICK_INTERVAL_SECONDS:
+                elapsed_ms = int((current_time - self.last_wait_time) * MS_PER_SECOND)
+                self.controller.handle_wait(elapsed_ms)
+                self.last_wait_time = current_time
+
+            snapshot = self.game.get_snapshot()
+            frame = self.renderer.render_frame(snapshot)
+
+            if frame is not None:
+                frame.show(self.window_name)
+
+            key = cv2.waitKey(FRAME_DELAY_MS) & 0xFF
+
+            if key == ord('q') or cv2.getWindowProperty(self.window_name, cv2.WND_PROP_VISIBLE) < 1:
+                self.running = False
+
+        cv2.destroyAllWindows()
