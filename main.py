ea02d5c396d6436f28f41707b5e1f25309152c84
Etty <etty70910@gmail.com>
2026-07-16 13:04:18 +0300

    source: board and pieces images

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from textTester.script_runner import run_script
+from view.renderer  import GameRenderer
+from view.GUI_runner import run_GUI
+from game_engine.game import KungFuChessGame
 
 def main():
-    run_script()
+    game=KungFuChessGame()
+    run_GUI(game)
+    # run_script()
 
 if __name__ == "__main__":
     main()
